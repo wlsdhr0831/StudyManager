1695255e08a23e60069d9521eb7af3e4a2d93b7c
성진옥 <wlsdhr0831@naver.com>
2021-08-10 21:19:45 +0900

    [init] frontend

---
+++ frontend/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  entry: {
+    main: './src/index',
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'serengeti_chatbot_tts.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: '/node_modules',
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-env', { targets: { esmodules: true, browsers: ['last 2 versions'] } }],
+            '@babel/preset-react',
+          ],
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};

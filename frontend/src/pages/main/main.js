7df5ee50307ebca8e5af10c77849efdda5213d33
성진옥 <wlsdhr0831@naver.com>
2021-08-26 21:17:57 +0900

    [feat] switch on/off by date

--- frontend/src/pages/main/main.js
+++ frontend/src/pages/main/main.js
@@ -1,18 +1,31 @@
 import React, { useState } from 'react';
-import PeopleList from '../../layout/main/peopleList/peopleList';
-import Search from '../../layout/main/search/search';
+import PeopleList from '../../layout/main/people/peopleList/peopleList';
+import Search from '../../layout/main/searchStudy/search/search';
 
 const Main = () => {
     const [ date, setDate ] = useState(new Date());
+    const [ today, setToday ] = useState(true);
 
     const changeDate = (e) => {
         setDate(e);
+
+        if(!sameDate(e, new Date())){
+            setToday(false);
+        }else{
+            setToday(true);
+        }
+    }
+
+    const sameDate = (a, b) => {
+        return a.getFullYear() === b.getFullYear()
+            && a.getMonth() === b.getMonth()
+            && a.getDate() === b.getDate();
     }
 
     return (
         <div>
             <Search date={date} changeDate={changeDate}/>
-            <PeopleList date={date} setDate={setDate}/>
+            <PeopleList date={date} changeDate={changeDate} today={today}/>
         </div>
     )
 }
